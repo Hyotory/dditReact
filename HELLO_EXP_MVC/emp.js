f34a9e63c0550e4aa5a47ad97d073734be26ae1d
HYO <jerroldhj@gmail.com>
2025-05-20 03:05:11 +0900

    250519 과제완

--- HELLO_EXP_MVC/emp.js
+++ HELLO_EXP_MVC/emp.js
@@ -49,6 +49,129 @@ app.get('/emp_list.do', (req, res) => {
     })
 });
 
+app.get('/emp_detail.do/:id', (req, res) => {
+    const e_id = req.params.id;
+    let sql = `
+                    select
+                        e_id, e_name, gen, addr
+                    from
+                        emp
+                    where e_id = ?
+                        `;
+
+    conn.query(sql, [e_id], function(err, rows, fields) {
+        if(err) {
+            console.log('DB오류 : ',err);
+            return res.status(500).send('DB오류');
+        }
+        if(rows.length === 0) {
+            return res.status(404).send('해당하는 행을 찾을 수 없다.');
+        }
+        console.log("DB에서 가져온 데이터:", rows);
+        res.render('emp_detail.ejs', { emp: rows[0] });
+    })
+});
+
+app.get('/emp_mod.do/:id', (req, res) => {
+    const e_id = req.params.id;
+    let sql = `
+                        select
+                            e_id, e_name, gen, addr
+                        from
+                            emp
+                        where e_id = ?
+                        `;
+
+    conn.query(sql, [e_id], function(err, rows, fields) {
+        if(err) {
+            console.log('DB오류 : ',err);
+            return res.status(500).send('DB오류');
+        }
+        if(rows.length === 0) {
+            return res.status(404).send('해당하는 행을 찾을 수 없다.');
+        }
+        console.log("DB에서 가져온 데이터:", rows);
+        res.render('emp_mod.ejs', { emp: rows[0] });
+    })
+});
+
+app.post('/emp_mod_act.do', (req, res) => {
+    const { e_id, e_name, gen, addr } = req.body;
+    let sql = `
+        UPDATE emp
+        SET e_name = ?, gen = ?, addr = ?
+        WHERE e_id = ?
+    `;
+
+    conn.query(sql, [e_name, gen, addr, e_id], function(err, result) {
+        if(err) {
+            console.log('DB오류 : ',err);
+            return res.render('emp_mod_act.ejs', {
+                success: false,
+                message: err.message,
+                e_id: e_id
+            });
+        }
+
+        res.render('emp_mod_act.ejs', {
+            success: true,
+            message: '',
+            e_id: e_id
+        });
+    });
+});
+
+app.get('/emp_del_act.do/:id', (req, res) => {
+    const e_id = req.params.id;
+
+    let sql = `
+                        DELETE FROM emp
+                        WHERE e_id = ?
+                        `;
+
+    conn.query(sql, [e_id], function(err, result) {
+        if(err) {
+        console.log('DB<UNK> : ',err);
+        return res.status(500).send('DB<UNK>');
+        }
+        res.render('emp_del_act.ejs', {
+            success: true,
+            message: '',
+            e_id: e_id
+        });
+    });
+});
+
+app.get('/emp_add.do', (req, res) => {
+    res.render('emp_add.ejs');
+});
+
+app.post('/emp_add_act.do', (req, res) => {
+    const { e_id, e_name, gen, addr } = req.body;
+
+    let sql = `
+    INSERT INTO emp (e_id, e_name, gen, addr)
+    VALUES (?, ?, ?, ?)
+    `;
+
+    conn.query(sql, [e_id, e_name, gen, addr], function(err, result) {
+        if(err) {
+            console.log('DB오류 : ', err);
+            return res.render('emp_add_act.ejs', {
+                success: false,
+                message: err.message,
+                e_id: e_id
+            });
+        }
+
+        res.render('emp_add_act.ejs', {
+            success: true,
+            message: '',
+            e_id: e_id
+        });
+    });
+})
+
 // 서버 시작
 app.listen(PORT, () => {
     console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
